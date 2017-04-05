545be8c282dea1eb3584c9ad8820a89b396c729c
Michael Hargreaves <code@digitalfeast.com.au>
2017-04-05 22:33:28 +1000

    implement gulp release task handling versioning and tags

--- gulpfile.js
+++ gulpfile.js
@@ -84,3 +84,66 @@ gulp.task('dev', ['clean'], function() {
 gulp.task('watch', function() { gulp.watch(_srcGlobs, ['build']); });
 gulp.task('watchdev', function() { gulp.watch(_srcGlobs, ['builddev']); });
 
+
+// RELEASE TASKS
+const bump      = require('gulp-bump');
+const gutil     = require('gulp-util');
+const git       = require('gulp-git');
+const runSequence   = require('run-sequence');
+const fs      = require('fs');
+
+
+// $ gulp release --type (major || minor || patch)
+gulp.task('release', function(callback) {
+runSequence(
+    'bump-version',
+    'commit-changes',
+    'push-changes',
+    'create-new-tag',
+    //'github-release',
+    function (error) {
+      if (error) {
+        console.log(error.message);
+      } else {
+        console.log('RELEASE FINISHED SUCCESSFULLY');
+      }
+      callback(error);
+    });
+});
+
+gulp.task('bump-version', function () {
+  var type = gutil.env.type || "patch";
+  console.log('Release patch type: ', type);
+// We hardcode the version change type to 'patch' but it may be a good idea to
+// use minimist (https://www.npmjs.com/package/minimist) to determine with a
+// command argument whether you are doing a 'major', 'minor' or a 'patch' change.
+  return gulp.src(['./package.json'])
+    .pipe(bump({type: type}).on('error', gutil.log))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('commit-changes', function () {
+  return gulp.src('.')
+    //.pipe(git.add())
+    .pipe(git.commit('[Prerelease] Bumped version number'));
+});
+
+gulp.task('push-changes', function (cb) {
+  git.push('origin', 'master', cb);
+});
+
+gulp.task('create-new-tag', function (cb) {
+  var version = getPackageJsonVersion();
+  git.tag(version, 'Created Tag for version: ' + version, function (error) {
+    if (error) {
+      return cb(error);
+    }
+    git.push('origin', 'master', {args: '--tags'}, cb);
+  });
+
+  function getPackageJsonVersion () {
+    // We parse the json file instead of using require because require caches
+    // multiple calls so the version number won't be updated
+    return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
+  };
+});
